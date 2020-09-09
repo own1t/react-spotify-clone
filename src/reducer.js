3ea5e5e757460dcc53a82e9ca19c2033768fa2fd
Ryan Kim <iawal86@gmail.com>
2020-09-09 11:57:03 -0700

    created header

--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,8 @@ export const initialState = {
   playlists: [],
   playing: false,
   item: null,
-  token:
-    "BQDw83ZdtLfjXEC6yAtMc7wKNKgjoV7wyiYL6H_ttmY9Ub4iDmlHp9Y5uaKIcRnBSz0c8DFjBmqpvSHc2litSnPBAragcVkXuEI5UJSPFvg8OLJvp76HVR6PuWGWopNCyXsNNvh7F32XVKuKEY1zsNVAWSAkUJImL1ddWU",
+  // token:
+  //   "BQDw83ZdtLfjXEC6yAtMc7wKNKgjoV7wyiYL6H_ttmY9Ub4iDmlHp9Y5uaKIcRnBSz0c8DFjBmqpvSHc2litSnPBAragcVkXuEI5UJSPFvg8OLJvp76HVR6PuWGWopNCyXsNNvh7F32XVKuKEY1zsNVAWSAkUJImL1ddWU",
 };
 
 const reducer = (state, action) => {
